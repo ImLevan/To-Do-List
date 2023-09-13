6c90fb773d42af15f594759dee19803c14fb4279
ImLevan <chianesevalentin@gmail.com>
2023-09-13 11:17:08 -0300

    commit inicialv3

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -1,6 +1,92 @@
+import { useEffect } from "react";
 
 
 const Clock = () => {
+    useEffect(() => {
+        const actualizarHora = function () {
+            const fecha: Date = new Date();
+            let horas: number = fecha.getHours();
+            let ampm: string;
+            const minutos: number = fecha.getMinutes();
+            const segundos: number = fecha.getSeconds();
+            const diaSemana: number = fecha.getDay();
+            const dia: number = fecha.getDate();
+            const mes: number = fecha.getMonth();
+            const año: number = fecha.getFullYear();
+    
+            const pHoras: HTMLElement | null = document.getElementById('horas');
+            const pAMPM: HTMLElement | null = document.getElementById('ampm');
+            const pMinutos: HTMLElement | null = document.getElementById('minutos');
+            const pSegundos: HTMLElement | null = document.getElementById('segundos');
+            const pDiaSemana: HTMLElement | null = document.getElementById('diaSemana');
+            const pDia: HTMLElement | null = document.getElementById('dia');
+            const pMes: HTMLElement | null = document.getElementById('mes');
+            const pAño: HTMLElement | null = document.getElementById('año');
+    
+            const semana: string[] = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'];
+    
+            if (pDiaSemana) {
+                pDiaSemana.textContent = semana[diaSemana];
+            }
+    
+            if (pDia) {
+                pDia.textContent = dia.toString();
+            }
+    
+            const meses: string[] = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+            if (pMes) {
+                pMes.textContent = meses[mes];
+            }
+            if (pAño) {
+                pAño.textContent = año.toString();
+            }
+    
+            if (horas >= 12) {
+                horas = horas - 12;
+                ampm = 'PM';
+    
+            } else {
+                ampm = 'AM';
+            }
+    
+            if (horas == 0) {
+                horas = 12;
+            }
+    
+            if (pHoras) {
+                pHoras.textContent = horas.toString();
+            }
+    
+            if (pAMPM) {
+                pAMPM.textContent = ampm;
+            }
+    
+            if (pMinutos) {
+                if (minutos < 10) {
+                    let m = String(minutos);
+                    m = "0" + minutos;
+                    pMinutos.textContent = m;
+                } else {
+                    pMinutos.textContent = minutos.toString();
+                }
+            }
+    
+            if (pSegundos) {
+                if (segundos < 10) {
+                    let s = String(segundos);
+                    s = "0" + segundos;
+                    pSegundos.textContent = s;
+                } else {
+                    pSegundos.textContent = segundos.toString();
+                }
+            }
+    
+        };
+    
+        actualizarHora();
+        setInterval(actualizarHora, 1000);
+    }, []);
+
     return (
         <div className="mx-auto" id="wrap">
             <div className="mx-auto text-center" id="widget">
@@ -10,13 +96,13 @@ const Clock = () => {
                     <p className="text-black px-1">de</p>
                     <p className="text-black px-1" id="mes"></p>
                     <p className="text-black px-1">del</p>
-                    <p className="text-black px-1 "id="año"></p>
+                    <p className="text-black px-1 " id="año"></p>
                 </div>
 
                 <div className="pl-36 py-5 flex mb-10" id="reloj">
                     <p className="text-black text-4xl" id="horas"></p>
                     <p className="text-black text-4xl">:</p>
-                    <p className="text-black text-4xl "id="minutos"></p>
+                    <p className="text-black text-4xl " id="minutos"></p>
                     <p className="text-black text-4xl">:</p>
                     <div className="flex " id="caja-segundos ">
                         <p className="text-black text-4xl py-0 my-0" id="segundos"></p>
